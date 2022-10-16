f9233e47e777f7228075ce4fa91e3a8843e13bd4
anonymous_pc <anonymous_pc_serbia@protonmail.com>
2022-10-16 18:01:44 -0400

    Added pdf export functionality.
    Added pdf export functionality.

--- cv-project/src/components/Nav.js
+++ cv-project/src/components/Nav.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Nav.css';
+import html2canvas from "html2canvas";
 
-export default function Nav() {
-    return (
-        <nav id='nav'>
-            <div id='logo'>
-                <span class="material-symbols-outlined">
-                    contact_page
-                </span>
-                <h1>CV Editor</h1>
-            </div>
-        </nav>
-    )
+export default class Nav extends Component {
+
+    exportAsImage = async (el, imageFileName) => {
+        const canvas = await html2canvas(document.getElementById('preview'));
+        const image = canvas.toDataURL("image/png", 1.0);
+        this.downloadImage(image, imageFileName);
+    };
+
+    downloadImage = (blob, fileName) => {
+        const fakeLink = window.document.createElement("a");
+        fakeLink.style = "display:none;";
+        fakeLink.download = fileName;
+
+        fakeLink.href = blob;
+
+        document.body.appendChild(fakeLink);
+        fakeLink.click();
+    };
+
+
+    render() {
+        return (
+            <nav id='nav'>
+                <div id='logo'>
+                    <span class="material-symbols-outlined">
+                        contact_page
+                    </span>
+                    <h1>CV Editor</h1>
+                    <button onClick={this.exportAsImage}>Print</button>
+                </div>
+            </nav>
+        );
+    }
 }
